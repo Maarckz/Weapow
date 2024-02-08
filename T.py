66f4ba1ebc6ddf26fb2fda65ba1c2600e4619564
Maarckz <48658125+Maarckz@users.noreply.github.com>
2024-02-08 10:05:43 -0300

    Update T.py

--- T.py
+++ T.py
@@ -11,3 +11,28 @@ print('ID DA ET:\n',hostname)
 print('NIP:\n',user)
 print('IPs:\n',IP)
 print('MACs:\n',MAC)
+
+
+# Definindo uma lista vazia para armazenar os dados
+$dados = @()
+
+# Obtendo o hostname
+$hostname = hostname
+
+# Obtendo o nome do usuário
+$user = whoami
+
+# Obtendo o endereço IP
+$IP = (ip addr show | Select-String -Pattern 'inet ' | ForEach-Object { $_ -split ' ' })[1].Split('/')[0]
+
+# Obtendo o endereço MAC
+$MAC = (ip link show | Select-String -Pattern 'link/ether' | ForEach-Object { $_ -split ' ' })[1]
+
+# Adicionando os dados à lista
+$dados += "ID DA ET:`n$hostname"
+$dados += "NIP:`n$user"
+$dados += "IPs:`n$IP"
+$dados += "MACs:`n$MAC"
+
+# Exibindo os dados
+Write-Output $dados
